0993ebf4093c9d37930b5f6111ab04b2ea3ba6ce
tomasfint3-ux <tomasfint3@gmail.com>
2026-08-23 23:12:43 -0500

    Añadi la clase Slotmachine y hice cambios al wheel

--- slotMachine/Wheel.java
+++ slotMachine/Wheel.java
@@ -1,28 +1,28 @@
-
 /**
  * Its a wheel with symbols, part of the slotMachine
- * 
- * @author Tomas Arevalo-Jose Parra 
+ *
+ * @author Tomas Arevalo-Jose Parra
  * @version 1.0
  */
+
 import java.util.ArrayList;
+
 public class Wheel
 {
     private int xPosition;
     private int yPosition;
     private ArrayList<Symbol> symbols;
     private boolean isVisible;
-    
 
     /**
      * Crea una rueda
      */
-    public Wheel(int xpos,int ypos)
+    public Wheel(int xpos, int ypos)
     {
-        symbols=new ArrayList<>();
-        isVisible =false;
-        this.xPosition=xpos;
-        this.yPosition=ypos;
+        symbols = new ArrayList<>();
+        isVisible = false;
+        this.xPosition = xpos;
+        this.yPosition = ypos;
     }
 
     /**
@@ -30,9 +30,10 @@ public class Wheel
      */
     public void addSymbols(String color)
     {
-        Symbol symbol=new Symbol(color,xPosition,yPosition);
+        Symbol symbol = new Symbol(color, xPosition, yPosition);
         symbols.add(symbol);
     }
+
     /**
      * Elimina el primer symbolo que encuentre con un determinado color
      */
@@ -45,52 +46,80 @@ public class Wheel
             }
         }
     }
+
     /**
      * Ubica en la rueda el simbolo del color indicado
      */
     public void placeSymbols(String color)
     {
         for (int i = 0; i < symbols.size(); i++) {
-            Symbol symbol=symbols.get(i);
+            Symbol symbol = symbols.get(i);
             symbol.makeInVisibleS();
+
             if (symbols.get(i).getColor().equalsIgnoreCase(color)) {
-                symbol=symbols.get(i);
                 symbol.makeVisibleS();
                 return;
             }
         }
     }
+
     /**
      * Hace girar a la rueda
      */
     public void spin()
     {
-        if (symbols.isEmpty()){
+        if (symbols.isEmpty()) {
             return;
         }
+
         int newIndex = (int) (Math.random() * symbols.size());
-        String findcolor=symbols.get(newIndex).getColor();
+        String findcolor = symbols.get(newIndex).getColor();
         placeSymbols(findcolor);
-        }
+    }
+
+    /**
+     * Devuelve los simbolos de la rueda
+     */
     public String[] getSymbols()
     {
         String[] colors = new String[symbols.size()];
+
         for (int i = 0; i < symbols.size(); i++) {
             colors[i] = symbols.get(i).getColor();
         }
+
         return colors;
     }
+
+    /**
+     * Devuelve el simbolo visible
+     */
     public String getVisibleSymbols()
     {
-        String color;
-        for (int i = 0; i < symbols.size(); i++)
-        {
-            Symbol symbol=symbols.get(i);
-            boolean visible=symbol.getvisibility();
-            if (visible = true){
-                return color=symbols.get(i).getColor();
+        for (int i = 0; i < symbols.size(); i++) {
+            Symbol symbol = symbols.get(i);
+
+            if (symbol.getvisibility()) {
+                return symbol.getColor();
             }
         }
+
         return "None";
+    }
+
+    /**
+     * Devuelve la posicion X de la rueda
+     */
+    public int getXPosition()
+    {
+        return xPosition;
+    }
+
+    /**
+     * Devuelve la posicion Y de la rueda
+     */
+    public int getYPosition()
+    {
+        return yPosition;
     }
 }
--- slotMachine/slotMachine.java
+++ slotMachine/slotMachine.java
@@ -1,33 +1,217 @@
-
 /**
- * Write a description of class slotMachine here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Its a slot machine with wheels and symbols
+ *
+ * @author Tomas Arevalo-Jose Parra
+ * @version 1.0
  */
-public class slotMachine
+
+import java.util.ArrayList;
+
+public class SlotMachine
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private ArrayList<Wheel> wheels;
+    private boolean visible;
+    private boolean ok;
 
     /**
-     * Constructor for objects of class slotMachine
+     * Crea una maquina tragamonedas
      */
-    public slotMachine()
+    public SlotMachine()
     {
-        // initialise instance variables
-        x = 0;
+        wheels = new ArrayList<>();
+        visible = false;
+        ok = true;
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * Añade una rueda a la maquina
      */
-    public int sampleMethod(int y)
+    public void addWheel(int xPos, int yPos)
     {
-        // put your code here
-        return x + y;
+        Wheel wheel = new Wheel(xPos, yPos);
+        wheels.add(wheel);
+    }
+
+    /**
+     * Elimina una rueda de la maquina
+     */
+    public void delWheel(int xPos, int yPos)
+    {
+        for (int i = 0; i < wheels.size(); i++) {
+            if (wheels.get(i).getXPosition() == xPos &&
+                wheels.get(i).getYPosition() == yPos) {
+                wheels.remove(i);
+                return;
+            }
+        }
+
+        ok = false;
+    }
+
+    /**
+     * Añade un simbolo a una rueda
+     */
+    public void addSymbol(int xPos, int yPos, String color)
+    {
+        for (int i = 0; i < wheels.size(); i++) {
+            if (wheels.get(i).getXPosition() == xPos &&
+                wheels.get(i).getYPosition() == yPos) {
+                wheels.get(i).addSymbols(color);
+                return;
+            }
+        }
+
+        ok = false;
+    }
+
+    /**
+     * Elimina un simbolo de todas las ruedas
+     */
+    public void delSymbol(String symbol)
+    {
+        for (int i = 0; i < wheels.size(); i++) {
+            wheels.get(i).delSymbols(symbol);
+        }
+    }
+
+    /**
+     * Ubica un simbolo en una rueda
+     */
+    public void placeSymbol(int wheel, String symbol)
+    {
+        if (wheel >= 0 && wheel < wheels.size()) {
+            wheels.get(wheel).placeSymbols(symbol);
+        }
+        else {
+            ok = false;
+        }
+    }
+
+    /**
+     * Hace girar una rueda
+     */
+    public void spin(int wheel)
+    {
+        if (wheel >= 0 && wheel < wheels.size()) {
+            wheels.get(wheel).spin();
+        }
+        else {
+            ok = false;
+        }
+    }
+
+    /**
+     * Consulta los simbolos de la maquina
+     */
+    public String[] symbols()
+    {
+        if (wheels.size() == 0) {
+            return new String[0];
+        }
+
+        return wheels.get(0).getSymbols();
+    }
+
+    /**
+     * Consulta la cantidad de simbolos diferentes
+     */
+    public int distinctSymbols()
+    {
+        String[] colors = symbols();
+        int cantidad = 0;
+
+        for (int i = 0; i < colors.length; i++) {
+            int repetido = 0;
+
+            for (int j = 0; j < i; j++) {
+                if (colors[i].equalsIgnoreCase(colors[j])) {
+                    repetido = 1;
+                }
+            }
+
+            if (repetido == 0) {
+                cantidad++;
+            }
+        }
+
+        return cantidad;
+    }
+
+    /**
+     * Consulta la configuracion actual de la maquina
+     */
+    public String[] configuration()
+    {
+        String[] colors = new String[wheels.size()];
+
+        for (int i = 0; i < wheels.size(); i++) {
+            colors[i] = wheels.get(i).getVisibleSymbols();
+        }
+
+        return colors;
+    }
+
+    /**
+     * Consulta si la configuracion es ganadora
+     */
+    public void isjackpot()
+    {
+        if (wheels.size() == 0) {
+            ok = false;
+            return;
+        }
+
+        String color = wheels.get(0).getVisibleSymbols();
+
+        for (int i = 1; i < wheels.size(); i++) {
+            if (!wheels.get(i).getVisibleSymbols().equalsIgnoreCase(color)) {
+                ok = false;
+                return;
+            }
+        }
+
+        ok = true;
+    }
+
+    /**
+     * Hace visible la maquina
+     */
+    public void makeVisible()
+    {
+        visible = true;
+    }
+
+    /**
+     * Hace invisible la maquina
+     */
+    public void makeInvisible()
+    {
+        visible = false;
+    }
+
+    /**
+     * Termina la maquina
+     */
+    public void exit()
+    {
+        makeInvisible();
+    }
+
+    /**
+     * Consulta si la ultima operacion fue correcta
+     */
+    public void ok()
+    {
+        if (ok == false) {
+            return;
+        }
+    }
+
+    /**
+     * Indica que ocurrio un error
+     */
+    public void Itserror(String message)
+    {
+        ok = false;
     }
 }
